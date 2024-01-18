f13567079b8eb04df89a23846471f69385108f67
Guido Pereira <stereopt@gmail.com>
2024-01-18 12:04:18 +0000

    feat: nextui & removed boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextUIProvider } from '@nextui-org/system';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import '@/styles/globals.css';
+
+import type { AppProps } from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
+};
+
+export default App;

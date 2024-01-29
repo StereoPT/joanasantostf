416649fcec8e252456bfff51e5abcabd4f8db93f
Guido Pereira <stereopt@gmail.com>
2024-01-29 15:10:24 +0000

    feat: added page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,14 @@ import { NextUIProvider } from '@nextui-org/system';
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <NextUIProvider>
+      <Head>
+        <title>Dra. Joana Santos</title>
+      </Head>
       <Component {...pageProps} />
     </NextUIProvider>
   );

1838ab62c44f4fa6e6f4e53ad0a596bfd54a29ba
Guido Pereira <stereopt@gmail.com>
2024-01-29 15:14:21 +0000

    feat: landing page iteration 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
+import AboutMe from '@/components/AboutMe/AboutMe';
+import Contact from '@/components/Contact/Contact';
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
+import Hero from '@/components/Hero/Hero';
+import Services from '@/components/Services/Services';
+import Team from '@/components/Team/Team';
+
 const LandingPage = () => {
-  return <main>Landing Page</main>;
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <AboutMe />
+      <Services />
+      <Team />
+      <Contact />
+      <Footer />
+    </main>
+  );
 };
 
 export default LandingPage;
